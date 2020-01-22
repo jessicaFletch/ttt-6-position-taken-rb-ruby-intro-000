045901bd8938cab477d466b1ee40f749385e9211
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 05:35:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,24 +1,4 @@
-# code your #position_taken? method here!
-=begin
-board = [" "," "," "," "," "," "," "," "," "]
 
-def position_taken?(board, index)
-  if board, index[0] =="nil"
-    return false 
-  
-  elsif board, index[0] == " "
-    return false 
-  else 
-    puts true 
-    
-  
- end
- end
- =end
- 
  
    ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
-end
-
-  ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
 end
